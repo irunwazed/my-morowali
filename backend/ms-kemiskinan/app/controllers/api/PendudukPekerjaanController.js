d80a64346fe1b0d5c91afcab51f5b2b2dd721799
irunwazed <irunwazed@gmail.com>
2022-12-06 18:27:40 +0800

    pekerjaan penduduk

--- backend/ms-kemiskinan/app/controllers/api/PendudukPekerjaanController.js
+++ backend/ms-kemiskinan/app/controllers/api/PendudukPekerjaanController.js
@@ -17,9 +17,28 @@ exports.controller = class PendudukPekerjaanController {
 
 
 	static async getData(req, res) {
-		var condition = {};
+		var condition = [
+			{
+				$lookup: {
+					from: 'penduduks',
+					localField: 'penduduk_id',
+					foreignField: '_id',
+					as: 'penduduk',
+				},
+			},
+			{ $unwind: "$penduduk" },
+			{
+				$lookup: {
+					from: 'pekerjaans',
+					localField: 'pekerjaan_id',
+					foreignField: '_id',
+					as: 'pekerjaan',
+				},
+			},
+			{ $unwind: "$pekerjaan" },
+		];
 		try{
-			let data = await paginate.find(req, 'penduduk_pekerjaan', condition);
+			let data = await paginate.aggregate(req, 'penduduk_pekerjaan', condition);
 			return res.send(data);
 		}catch(err){
 			return res.status(500).send({
@@ -32,12 +51,33 @@ exports.controller = class PendudukPekerjaanController {
 	static async getOneData(req, res) {
 		try{
 			let id = req.params.id;
-			let data = await table.findById(id);
-			if (!data) return res.status(400).send({
+			var condition = [
+				{
+					$lookup: {
+						from: 'penduduks',
+						localField: 'penduduk_id',
+						foreignField: '_id',
+						as: 'penduduk',
+					},
+				},
+				{ $unwind: "$penduduk" },
+				{
+					$lookup: {
+						from: 'pekerjaans',
+						localField: 'pekerjaan_id',
+						foreignField: '_id',
+						as: 'pekerjaan',
+					},
+				},
+				{ $unwind: "$pekerjaan" },
+				{ $match: { _id: db.mongoose.Types.ObjectId(id) } }
+			];
+			let data = await table.aggregate(condition);
+			if (!data[0]) return res.status(400).send({
 						statusCode: 400,
 						message: "Not found data with id " + id,
 					});
-			else return res.send({statusCode: 200, data: data});
+			else return res.send({statusCode: 200, data: data[0]});
 		}catch(err){
 			return res.status(500).send({
 				statusCode: 500,
