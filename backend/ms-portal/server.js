ec20d9c6605666b8936a714589f7269f058d2991
irunwazed <irunwazed@gmail.com>
2022-12-06 13:58:46 +0800

    portal

--- backend/ms-portal/server.js
+++ backend/ms-portal/server.js
@@ -5,12 +5,35 @@ const fs = require("fs");
 const host = '127.0.0.1';
 const port = 3030;
 
-fs.readFile('./index.html', (error, html)=> {
-  if(error) throw error;
+// const tes = async () => {
+//   console.log(data);
+// }
+// tes();
 
-  http.createServer((req, res)=> {
-    res.writeHead(200, {"Content-Type": "text/html"});
-    res.write(html);
-    res.end();
-  }).listen(port, host);
-})
+// fs.readFile('./index.html', (error, html)=> {
+//   if(error) throw error;
+
+//   console.log(html)
+// })
+
+// fs.readFile('./index.html', (error, html)=> {
+//   if(error) throw error;
+
+//   http.createServer((req, res)=> {
+//     res.writeHead(200, {"Content-Type": "text/html"});
+//     res.write(html);
+//     res.end();
+//   }).listen(port, host);
+// })
+
+const requestListener = (req, res) => {
+  let html = fs.readFileSync('./index.html');
+  res.writeHead(200, {"Content-Type": "text/html"});
+  res.write(html);
+  res.end();
+};
+
+const server = http.createServer(requestListener);
+server.listen(port, host, () => {
+    console.log(`Server is running on http://${host}:${port}`);
+});
