948378590649be31cd41577da3cb4e5f2ba2b738
irunwazed <irunwazed@gmail.com>
2022-12-06 14:13:40 +0800

    portal

--- backend/ms-portal/server.js
+++ backend/ms-portal/server.js
@@ -5,26 +5,6 @@ const fs = require("fs");
 const host = '127.0.0.1';
 const port = 3030;
 
-// const tes = async () => {
-//   console.log(data);
-// }
-// tes();
-
-// fs.readFile('./index.html', (error, html)=> {
-//   if(error) throw error;
-
-//   console.log(html)
-// })
-
-// fs.readFile('./index.html', (error, html)=> {
-//   if(error) throw error;
-
-//   http.createServer((req, res)=> {
-//     res.writeHead(200, {"Content-Type": "text/html"});
-//     res.write(html);
-//     res.end();
-//   }).listen(port, host);
-// })
 
 const requestListener = (req, res) => {
   let html = fs.readFileSync('./index.html');
