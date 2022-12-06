e26f2ec30362ed73cc969e1b231e76fcce05883b
HafidDH <hafiddwih@gmail.com>
2022-12-06 16:06:30 +0700

    12

--- backend/ms-organisasi/app/config/db.config.js
+++ backend/ms-organisasi/app/config/db.config.js
@@ -2,5 +2,5 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 module.exports = {
-  url: "mongodb://localhost:27017/"+process.env.MONGODB_DB
+  url: "mongodb://127.0.0.1:27017/"+process.env.MONGODB_DB
 };
