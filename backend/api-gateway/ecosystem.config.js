13ca0d7186da1a5b000dca4ae0dcf23661d56df0
irunwazed <irunwazed@gmail.com>
2022-12-06 11:27:23 +0800

    perbaikanb

--- backend/api-gateway/ecosystem.config.js
+++ backend/api-gateway/ecosystem.config.js
@@ -2,13 +2,13 @@ module.exports = {
   apps: [{
     name: "API-GATEWAY",
     script: "./server.js",
-    // watch: true,
-    // log_date_format: "YYYY/MM/DD HH:mm Z",
-    // error_file: "./storages/logs/stderr.log",
-    // out_file: "./storages/logs/stdout.log",
+    watch: true,
+    log_date_format: "YYYY/MM/DD HH:mm Z",
+    error_file: "./storages/logs/stderr.log",
+    out_file: "./storages/logs/stdout.log",
     // instances: 1,
     // max_memory_restart: "512M",
-    // autorestart: true,
+    autorestart: true,
     // merge_logs: true,
   }]
 }
