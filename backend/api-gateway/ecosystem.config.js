d98813e75d89fed8b3b83d8303021a8cc7c883eb
irunwazed <irunwazed@gmail.com>
2022-12-09 13:14:06 +0800

    excel

--- backend/api-gateway/ecosystem.config.js
+++ backend/api-gateway/ecosystem.config.js
@@ -4,6 +4,7 @@ module.exports = {
     script: "./server.js",
     watch: true,
     log_date_format: "YYYY/MM/DD HH:mm Z",
+    ignore_watch : ["node_modules"],
     // error_file: "./storages/logs/stderr.log",
     // out_file: "./storages/logs/stdout.log",
     instances: 1,
