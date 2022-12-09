4da9682e6d2121a1edf75cc90ca889d6b3e9999a
irunwazed <irunwazed@gmail.com>
2022-12-09 08:47:53 +0800

    parser

--- backend/api-gateway/server.js
+++ backend/api-gateway/server.js
@@ -1,14 +1,21 @@
 const express = require('express');
 const { createProxyMiddleware } = require('http-proxy-middleware');
 const cors = require('cors');
+const bodyParser = require("body-parser");
 const dotenv = require('dotenv');
 dotenv.config();
 
 const app = express();
 const router = express.Router()
 const port = process.env.APP_PORT || 3000;
-app.use(cors())
 
+app.use(cors())
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  }),
+  bodyParser.json(),
+)
 const setProxy = require('./middlewares/proxy');
 const setAuth = require('./middlewares/auth');
 const ROUTES = require('./config/routes');
